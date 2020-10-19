4f9e66df96dc51d8a01bb48dc9341606b9afb0e3
Kent0129 <70558805+Kent0129@users.noreply.github.com>
2020-10-19 14:59:02 +0900

    turnsテーブルの作成

--- db/migrate/20201019055102_create_turns.rb
+++ db/migrate/20201019055102_create_turns.rb
@@ -1,7 +1,10 @@
 class CreateTurns < ActiveRecord::Migration[6.0]
   def change
     create_table :turns do |t|
-
+      t.string :name,     null: false
+      t.integer :number,  null: false
+      t.integer :status,  default: "順番待ち中"
+      t.timestamps
       t.timestamps
     end
   end
