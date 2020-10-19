9bfe4ad6e669c0a2ec500ab109251a755c17c01b
Kent0129 <70558805+Kent0129@users.noreply.github.com>
2020-10-19 17:52:30 +0900

    newとcreateアクションを設定

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -7,5 +7,26 @@ class TurnsController < ApplicationController
     @wait_people = Turn.where(status: 0).count
     @absence_people = Turn.where(status: 2).count
   end
-  
+
+
+  def new
+    @examinations = Turn.new
+    @today_num_end = Turn.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).maximum(:number)
+    @next_num =  @today_num_end + 1
+  end
+
+  def create
+    @turn = Turn.new(turn_params)
+    if @turn.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def turn_params
+    params.permit(:name, :number, :status)
+  end
+
 end
