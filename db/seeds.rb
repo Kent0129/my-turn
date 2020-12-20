05c36ddf3f1b86b87063454d7b97193989651571
Kent0129 <70558805+Kent0129@users.noreply.github.com>
2020-12-21 05:31:38 +0900

    seedsの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
-User.create!(user_name:  "管理者",
-  email: "admin@example.jp",
-  password:  "11111111",
-  password_confirmation: "11111111",
-  admin: true)
+User.create!(
+  [
+    {
+      user_name:  "管理者",
+      email: "admin@example.jp",
+      password:  "11111111",
+      password_confirmation: "11111111",
+      admin: true
+    },
+    {
+      user_name:  "番号発券",
+      email: "issue@example.jp",
+      password:  "11111111",
+      password_confirmation: "11111111",
+      admin: false
+    }
+  ]
+)
+        
