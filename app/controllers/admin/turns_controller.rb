f3c100e60dd921dee527590a4980125209f07582
Kent0129 <kato.kent0129@gmail.com>
2020-12-31 21:06:27 +0900

    indexアクションを定義

--- app/controllers/admin/turns_controller.rb
+++ app/controllers/admin/turns_controller.rb
@@ -1,2 +1,12 @@
 class Admin::TurnsController < ApplicationController
+  before_action :admin_user
+
+  def index
+    @turns = User.all.order("created_at DESC")
+  end
+
+  private
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
