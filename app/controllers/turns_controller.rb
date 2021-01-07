59be1793dcc42b63181883b047dbb8c17a261292
Kent0129 <kato.kent0129@gmail.com>
2021-01-08 00:22:35 +0900

    当日の番号が表示されるように変更

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,19 +1,20 @@
 class TurnsController < ApplicationController
   before_action :now_status, only: [:index, :show]
+  before_action :today_create, only:[:index, :show, :new]
   before_action :set_turn, only: [:update, :show]
   before_action :authenticate_user!, only: [:new, :create]
   before_action :admin_check, only: [:update]
   before_action :qrcode, only:[:show]
 
   def index
-    @wait = Turn.where(status: "待ち")
-    @absence = Turn.where(status: "不在")
+    @wait = @today.where(status: "待ち")
+    @absence = @today.where(status: "不在")
   end
 
 
   def new
     @examinations = Turn.new
-    @today_num_end = Turn.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).maximum(:number)
+    @today_num_end = @today.maximum(:number)
   end
 
   def create
@@ -33,10 +34,10 @@ class TurnsController < ApplicationController
   end
 
   def show
-    @myturn = Turn.where(status: "診察").ids.index(@turn.id)
-    @end = Turn.where(status: "終了").ids.include?(@turn.id)
-    @count = Turn.where(status: "待ち").ids.index(@turn.id)
-    @absence = Turn.where(status: "不在").ids.include?(@turn.id)
+    @myturn = @today.where(status: "診察").ids.index(@turn.id)
+    @end = @today.where(status: "終了").ids.include?(@turn.id)
+    @count = @today.where(status: "待ち").ids.index(@turn.id)
+    @absence = @today.where(status: "不在").ids.include?(@turn.id)
   end
 
   private
@@ -48,10 +49,15 @@ class TurnsController < ApplicationController
     @turn = Turn.find(params[:id])
   end
 
+  def today_create
+    @today = Turn.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+  end
+
   def now_status
-    @now = Turn.where(status: "診察").first
-    @wait_people = Turn.where(status: "待ち").count
-    @absence_people = Turn.where(status: "不在").count
+    @today = Turn.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    @now = @today.where(status: "診察").first
+    @wait_people = @today.where(status: "待ち").count
+    @absence_people = @today.where(status: "不在").count
   end
 
   def admin_check
